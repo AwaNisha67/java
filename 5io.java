3ecfae3e8251e9c6b392f954cf7e777668b760d6
Awanisha <130660671+AwaNisha67@users.noreply.github.com>
2024-09-10 11:58:29 +0530

    Create 5io.java

---
+++ 5io.java
@@ -0,0 +1,15 @@
+/*5. Enter the double from user using Scanner class*/
+import java.util.Scanner;
+class Main{
+	public static void main(String ap[]){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a Double number:");
+		Double x = input.nextDouble();
+		System.out.println("You have entered:" + x);
+	}
+
+}
+//output
+Enter a Double number:
+2555.2323
+You have entered:2555.2323
