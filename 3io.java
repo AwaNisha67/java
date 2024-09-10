2a24e85a31b509930df75b113a55b5752cde6725
Awanisha <130660671+AwaNisha67@users.noreply.github.com>
2024-09-10 11:53:08 +0530

    Create 3io.java

---
+++ 3io.java
@@ -0,0 +1,11 @@
+/*3. Enter the integer from user using Scanner class*/
+import java.util.Scanner;
+class Main{
+	public static void main(String ap[]){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer");
+		int x = input.nextInt();
+		System.out.println("You have entered:" + x);
+	}
+
+}
