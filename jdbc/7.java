b05106969f0f11cbdfcfd557430d2d105a980930
Awanisha <130660671+AwaNisha67@users.noreply.github.com>
2024-09-17 22:44:28 +0530

    Update 7.java

--- jdbc/7.java
+++ jdbc/7.java
@@ -3,19 +3,23 @@
 import java.sql.*;
 
 class Main{
-	public static void main(String ap[]) throws ClassNotFoundException,java.sql.SQLException{
-		//load the drivers
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		//connection 
-		Connection c1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/javadb","root","9780314063");
-		System.out.println("Connection has been established");
-		
-		Statement s1 = c1.createStatement();
-		ResultSet rs = s1.executeQuery("select * from student");
-		while(rs.next()){
+	public static void main(String ap[]) throws ClassNotFoundException, java.sql.SQLException{
+	String path = "jdbc:mysql://localhost:3306/javadb";
+	String user = "root";
+	String pass = "9780314063";
 
-		System.out.println(rs.getInt(1));
-		System.out.println(rs.getString(2));
-		}
+	//load the drivers
+	Class.forName("com.mysql.cj.jdbc.Driver");
+
+	//establish a connection
+	Connection c1 = DriverManager.getConnection(path,user,pass);
+	
+	Statement stmt = c1.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+	ResultSet rs = stmt.executeQuery("select * from student");
+	
+	System.out.println(rs.next());//true
+	System.out.println(rs.getInt(1));
+	System.out.println(rs.getString(2));
 	}
+
 }
